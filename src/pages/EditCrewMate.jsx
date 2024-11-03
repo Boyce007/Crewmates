6a4eb87c1ed575d495b589b366da8f81facb576d
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-11-03 16:20:10 -0500

    created getcrewmate function to fetch api data and update state varaible

--- src/pages/EditCrewMate.jsx
+++ src/pages/EditCrewMate.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useState,useEffect } from 'react';
+import { supabase } from '../client';
 const EditCrewMate = () => {
     const {id} = useParams();
     const [crewmate,setCrewmate] = useState({id:null,name:"",speed:0,color:""})
-    
-
+    useEffect(()=>{
+        const getCrewmate = async () => {
+            const {data} = await supabase
+            .from("Crewmates")
+            .select('name','speed','color','id')
+            .eq('id',id)
+            
+            setCrewmate(data)
+        }
+        getCrewmate()
+        
+    },[])
+    console.log(crewmate)
   return (
     <div>
-      <h1>{id} </h1>
+      <h1>Edit a Crewmate </h1>
     </div>
   )
 }
